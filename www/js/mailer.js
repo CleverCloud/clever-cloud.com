64dd082a719f7b5f1b38260da92f6814bc823835
Clément Nivolle <clement@nivolle.com>
2012-03-03 19:09:51 +0100

    Ajout de nouvelles fonctionnalités dans le code JavaScript mailer

---
+++ www/js/mailer.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('#signup-form').submit(function() {
+        var mail = $("#signup-email").val();
+
+        $.ajax({
+            type: "POST",
+            url: "http://www.clever-cloud.com/resources/mail/add",
+            data: "betasignup=" + mail,
+			statusCode: {
+				200: function() {
+	                //ajouter un p && supprimer des éléments
+	                $('#signup-form').empty();
+	                $('#signup-form').append('<p class="subscribe_success">Test</p>');
+	            },
+				401: function() {
+					//ajouter un p && supprimer des éléments
+	                $('#signup-form').empty();
+	                $('#signup-form').append('<p class="subscribe_fail">Mail alreaduy existe</p>');
+			    },
+				404: function() {
+	                //ajouter un p && supprimer des éléments
+	                $('#signup-form').empty();
+	                $('#signup-form').append('<p class="subscribe_fail">404 error. Sorry about that</p>');
+	            },
+				500: function() {
+	                //ajouter un p && supprimer des éléments
+	                $('#signup-form').empty();
+	                $('#signup-form').append('<p class="subscribe_fail">Oops, somethiung went wrong… and we have no whale.</p>');
+	            },
+			  }
+        });
+        return false;
+    });
+});
