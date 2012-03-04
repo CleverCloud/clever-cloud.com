83d6e0790a203734a674001f9fc037f219c3a091
Clément Nivolle <clement@nivolle.com>
2012-03-04 15:20:34 +0100

    deleted signup h2 in doc.css because we have no signup h2

--- www/js/mailer.js
+++ www/js/mailer.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
 				200: function() {
 	                //ajouter un p && supprimer des éléments
 	                $('#signup-form').empty();
-	                $('#signup-form').append('<p class="subscribe_success">Test</p>');
+	                $('#signup-form').append('<p class="subscribe_success">Mail ok</p>');
 	            },
 				401: function() {
 					//ajouter un p && supprimer des éléments
 	                $('#signup-form').empty();
-	                $('#signup-form').append('<p class="subscribe_fail">Mail alreaduy existe</p>');
+	                $('#signup-form').append('<p class="subscribe_fail">Mail already exists existe</p>');
 			    },
 				404: function() {
 	                //ajouter un p && supprimer des éléments
