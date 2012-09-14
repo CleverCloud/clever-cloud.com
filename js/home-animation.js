7005bbbb3cd8f8644a30513d750a436399a99ee8
Clément Nivolle <clement@nivolle.com>
2012-09-14 17:25:28 +0200

    animation + new slide

--- js/home-animation.js
+++ js/home-animation.js
@@ -3,7 +3,9 @@ var current = 0;
 	
 function graphscroll(){
 	current -=1;
-	$("#home-page").css("background-position-x",current+"px");
-	$("#home-fuel-wrap").css("background-position-x",current+"px");
+	$("#home-page").css("background-position", current+"px 100%" );
+	$("#home-fuel-wrap").css("background-position",current+"px 00%");
+	$("#home-page").css("background-position");
+	//console.log('$("#home-page").css("background-position);
 }
 var init = setInterval("graphscroll()", scrollSpeed); 
