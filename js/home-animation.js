d4e82c54201d09181f6e3bedd34cc9e53d115dbe
Simon Chatteleyn <kilik2049@gmail.com>
2012-10-10 16:34:53 +0200

    start color detection

--- js/home-animation.js
+++ js/home-animation.js
@@ -12,17 +12,42 @@
 
 // Get color of canvas
 
-/*function rgbToHex(r, g, b) {
+function rgbToHex(r, g, b) {
     if (r > 255 || g > 255 || b > 255)
         throw "Invalid color component";
     return ((r << 16) | (g << 8) | b).toString(16);
 }
 
-var c = getElementById('canvas-1').getContext('2d');
-var p = c.getImageData(x, y, 1, 1).data; 
-var hex = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+function getColor(x, y) {
+  var c = document.getElementById('canvas-1').getContext('2d');
+  var p = c.getImageData(x, y, 1, 1).data; 
+  var hex = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+  
+  var pplus = c.getImageData(x, y+1,1,1).data;
+  var hexplus = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+  var pminus = c.getImageData(x, y-1,1,1).data;
+  var hexminus = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+  var pplusmin = c.getImageData(x-1, y-1,1,1).data;
+  var hexplusmin = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+  var ppminmin = c.getImageData(x-1, y-1,1,1).data;
+  var hexminmin = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
 
-if (hex_right > "efefef") {
+  if (pminus != p) {
+    if (pplusmin == p) {
+      // descendre
+    }
+    else if (pplusmin != p) {
+      // monter
+    }
+    else if (pplusmin != p && pminmin != p) {
+    	// pas bouger
+    }
+  }
+  else if (pplus != p) {getColor(x, y+1)}
+  else {getColor(x, y-1)}
+}
+
+/*if (hex_right > "efefef") {
   // monter
 }
 else if (hex_left > "efefef" || hex_down <= "efefef") {
@@ -65,7 +90,7 @@ function launch() {
       store["wave3"+i].scaleX = 1;
       store["wave3"+i].scaleY = 2;
       store["wave3"+i].x = canvas.width - 400 * i;
-      store["wave3" + i].y = 50; 
+      store["wave3" + i].y = 70; 
 
       createjs.Tween.get(store["wave" + i], {loop:true}).to({x:canvas.width - (300 * (Math.floor(size) + i)), y:0}, 100000);
       createjs.Tween.get(store["wave2"+i], {loop:true}).to({x:canvas.width - (200 * (Math.floor(size2) + i)), y:0}, 80000);
