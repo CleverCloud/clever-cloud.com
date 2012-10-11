eba865a7b7566c3dcf457abb030a22cff2473da5
Simon Chatteleyn <kilik2049@gmail.com>
2012-10-11 14:58:00 +0200

    refactor canvas color. Fix wave bug

--- js/home-animation.js
+++ js/home-animation.js
@@ -12,49 +12,52 @@
 
 // Get color of canvas
 
-function rgbToHex(r, g, b) {
-    if (r > 255 || g > 255 || b > 255)
-        throw "Invalid color component";
-    return ((r << 16) | (g << 8) | b).toString(16);
-}
+function movingPoints() {
+  var x1 = 0;
+  var x2 = 0;
+  function rgbToHex(r, g, b) {
+      if (r > 255 || g > 255 || b > 255)
+          throw "Invalid color component";
+      return ((r << 16) | (g << 8) | b).toString(16);
+  }
 
-function getColor(x, y) {
-  var c = document.getElementById('canvas-1').getContext('2d');
-  var p = c.getImageData(x, y, 1, 1).data; 
-  var hex = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-  
-  var pplus = c.getImageData(x, y+1,1,1).data;
-  var hexplus = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-  var pminus = c.getImageData(x, y-1,1,1).data;
-  var hexminus = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-  var pplusmin = c.getImageData(x-1, y-1,1,1).data;
-  var hexplusmin = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-  var ppminmin = c.getImageData(x-1, y-1,1,1).data;
-  var hexminmin = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+  function getColor(x, y) {
+    var c = document.getElementById('canvas-1').getContext('2d');
+    var p = c.getImageData(x, y, 1, 1).data; 
+    var hex = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+    return hex;
+  }
 
-  if (pminus != p) {
-    if (pplusmin == p) {
-      // descendre
-    }
-    else if (pplusmin != p) {
-      // monter
-    }
-    else if (pplusmin != p && pminmin != p) {
-    	// pas bouger
+  function move(x) {
+    for (y = 100; y > 0; y--) {  
+      var p = getColor(x,y);
+      var pminus = getColor(x,y-1);
+      var pplus = getColor(x,y+1);
+
+      if (pminus != p) {
+        var pplusmin = getColor(x+1, y-1);
+        var pminmin = getColor(x-1, y-1);
+        if (pplusmin == p) {
+          // descendre
+        }
+        else if (pplusmin != p) {
+          // monter
+        }
+        else if (pplusmin != p && pminmin != p) {
+          // pas bouger
+        }
+      }
+      else if (pplus != p) {y = y +1; continue;}
+      else {continue;}
     }
   }
-  else if (pplus != p) {getColor(x, y+1)}
-  else {getColor(x, y-1)}
+  move(x1);
+  move(x2);
 }
 
-/*if (hex_right > "efefef") {
-  // monter
-}
-else if (hex_left > "efefef" || hex_down <= "efefef") {
- // descendre
-}*/
+// create waves
 
-function init(){
+function init() {
   var stage;
 
   var store = {
@@ -63,47 +66,46 @@ function init(){
     }
   };
 
-function launch() {
-  var canvas = document.getElementById('canvas-1');
-  stage = new createjs.Stage(canvas);
-  stage.autoClear = true;
-  var size = canvas.width / 300 + 1;
-  var size2 = canvas.width / 200 + 1;
-  var size3 = canvas.width / 400 + 1
-  // original image size / scaleX = size
-  var img = new Image();
-  img.src = "../img/vague3.png";
-  img.style.opacity = "0.5";
-  img.onload = function(e){
-    for (var i = -size; i < size; i++) {
-      store["wave" + i]	= new createjs.Bitmap(e.target);
-      store["wave" + i].scaleX = 0.75;
-      store["wave" + i].scaleY = 2;
-      store["wave" + i].x = canvas.width - 300 * i;
-      
-      store["wave2"+i] = new createjs.Bitmap(e.target);
-      store["wave2"+i].scaleX = 0.5;
-      store["wave2"+i].scaleY = 2;
-      store["wave2"+i].x = canvas.width - 200 * i;
+  function launch() {
+    var canvas = document.getElementById('canvas-1');
+    stage = new createjs.Stage(canvas);
+    stage.autoClear = true;
+    var size = canvas.width / 300 + 1;
+    var size2 = canvas.width / 280 +1;
+    var size3 = canvas.width / 500 + 1;
+    // original image size / scaleX = size
+    var img = new Image();
+    img.src = "../img/vague3.png";
+    img.style.opacity = "0.5";
+    img.onload = function(e){
+      for (var i = -size; i < size; i++) {
+        store["wave" + i]	= new createjs.Bitmap(e.target);
+        store["wave" + i].scaleX = 0.75;
+        store["wave" + i].scaleY = 2;
+        store["wave" + i].x = canvas.width - 300 * i;
+        
+        store["wave2"+i] = new createjs.Bitmap(e.target);
+        store["wave2"+i].scaleX = 0.70;
+        store["wave2"+i].scaleY = 2;
+        store["wave2"+i].x = canvas.width - 280 * i;
 
-      store["wave3"+i] = new createjs.Bitmap(e.target);
-      store["wave3"+i].scaleX = 1;
-      store["wave3"+i].scaleY = 2;
-      store["wave3"+i].x = canvas.width - 400 * i;
-      store["wave3" + i].y = 70; 
+        store["wave3"+i] = new createjs.Bitmap(e.target);
+        store["wave3"+i].scaleX = 1;
+        store["wave3"+i].scaleY = 2;
+        store["wave3"+i].x = canvas.width - 500 * i;
 
-      createjs.Tween.get(store["wave" + i], {loop:true}).to({x:canvas.width - (300 * (Math.floor(size) + i)), y:0}, 100000);
-      createjs.Tween.get(store["wave2"+i], {loop:true}).to({x:canvas.width - (200 * (Math.floor(size2) + i)), y:0}, 80000);
-      createjs.Tween.get(store["wave3"+i], {loop:true}).to({x:canvas.width - (400 * (Math.floor(size3) + i)), y:0}, 60000);
+        createjs.Tween.get(store["wave" + i], {loop:true}).to({x:canvas.width - (300 * (Math.floor(size) + i)), y:0}, 100000);
+        createjs.Tween.get(store["wave2"+i], {loop:true}).to({x:canvas.width - (280 * (Math.floor(size2) + i)), y:0}, 100000);
+        createjs.Tween.get(store["wave3"+i], {loop:true}).to({x:canvas.width - (500 * (Math.floor(size3) + i)), y:0}, 60000);
 
-      stage.addChild(store["wave" + i]);
-      stage.addChild(store["wave2"+i]);
-      stage.addChild(store["wave3"+i]);
+        stage.addChild(store["wave" + i]);
+        stage.addChild(store["wave2" + i]);
+        stage.addChild(store["wave3" + i]);
 
-      stage.update();
+        stage.update();
+      }
+      createjs.Ticker.addListener(store);
     }
-    createjs.Ticker.addListener(store);
   }
-}
 launch();
 }
