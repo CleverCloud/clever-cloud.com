c60d7721b0eac05df7efb22e3585f02875c59459
Clément Nivolle <clement@nivolle.com>
2012-10-14 17:38:05 +0200

    changed speed

--- js/home-animation.js
+++ js/home-animation.js
@@ -11,9 +11,9 @@
 //var init = setInterval("graphscroll()", scrollSpeed); 
 
 // Get color of canvas
-
+/*
 function movingPoints() {
-	/*
+
   var d1 = $('div.span8 div.row:last div.span4:first p:first');
   var d2 = $('div.span8 div.row:last div.span4:last p:first');
   var x1 = d1.offset().top - d1.height() + 2;
@@ -33,7 +33,7 @@ function movingPoints() {
     var hex = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
     return h2d(hex);
   }
-  */
+
   function move(x) {
     for (y = 100; y > 0; y--) {  
       var p = getColor(x,y);
@@ -58,7 +58,7 @@ function movingPoints() {
   move(x1);
   move(x2);
 }
-
+*/
 // create waves
 
 function init() {
@@ -76,11 +76,10 @@ function init() {
     stage.autoClear = true;
     var size = canvas.width / 300 + 1;
     var size2 = canvas.width / 280 +1;
-    var size3 = canvas.width / 500 + 1;
+    var size3 = canvas.width / 340 + 1;
     // original image size / scaleX = size
     var img = new Image();
     img.src = "../img/vague3.png";
-    img.style.opacity = "0.5";
     img.onload = function(e){
       for (var i = -size; i < size; i++) {
         store["wave" + i]	= new createjs.Bitmap(e.target);
@@ -98,9 +97,9 @@ function init() {
         store["wave3"+i].scaleY = 2;
         store["wave3"+i].x = canvas.width - 500 * i;
 
-        createjs.Tween.get(store["wave" + i], {loop:true}).to({x:canvas.width - (300 * (Math.floor(size) + i)), y:0}, 100000);
-        createjs.Tween.get(store["wave2"+i], {loop:true}).to({x:canvas.width - (280 * (Math.floor(size2) + i)), y:0}, 75000);
-        createjs.Tween.get(store["wave3"+i], {loop:true}).to({x:canvas.width - (500 * (Math.floor(size3) + i)), y:0}, 60000);
+        createjs.Tween.get(store["wave" + i], {loop:true}).to({x:canvas.width - (300 * (Math.floor(size) + i)), y:0}, 242704);
+        createjs.Tween.get(store["wave2"+i], {loop:true}).to({x:canvas.width - (280 * (Math.floor(size2) + i)), y:0}, 199000);
+        createjs.Tween.get(store["wave3"+i], {loop:true}).to({x:canvas.width - (500 * (Math.floor(size3) + i)), y:0}, 150705);
 
         stage.addChild(store["wave" + i]);
         stage.addChild(store["wave2" + i]);
