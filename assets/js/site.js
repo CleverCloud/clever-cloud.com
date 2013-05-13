d74c59d85ecaea2fe33128527e5165fa288616fd
Quentin ADAM <quentin.adam@waxzce.org>
2013-05-13 10:46:17 +0200

    wip price

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,5 +3,19 @@ $(function(){
 });
 
 $(function(){
-   $("#range_slider").rangeSlider();
+   $("#range_slider").editRangeSlider();;
+});
+
+
+
+// 
+
+$(function(){
+   $.ajax({
+      url:'https://console.clever-cloud.com/ccapi/dev/instances',
+      datatype:'jsonp',
+      onSuccess:function(i){
+         console.log(i);
+      }
+   });
 });
