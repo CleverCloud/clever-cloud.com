f41c69a72305fe62ffc816bf361d5dd742dc8c77
Clément Nivolle <clement@nivolle.com>
2013-05-27 14:50:27 +0200

    added sitemap generation

--- site.hs
+++ site.hs
@@ -1,14 +1,18 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Applicative ((<$>))
+import           Control.Applicative ((<$>), (<*>))
 import           Control.Monad       (forM_)
 import           Data.Monoid         (mappend, mempty)
 import           Data.Maybe          (catMaybes, fromMaybe, listToMaybe, maybeToList)
 import qualified Data.Map            as M
-
+import           Data.Time.Clock
+import           Data.Time.Clock.POSIX
+import           Data.Time.Format
+import           System.Posix.Files
+import           System.Locale
 import           Utils
 import           RouteFactories
-
+import           Data.List
 import           Hakyll
 
 --------------------------------------------------------------------------------
@@ -43,6 +47,8 @@ main = hakyll $ do
     match "fr/index.md" $ do
         route   $ setExtension "html"
         compile copyFileCompiler
+
+    createSitemap $ loadAll ("en/**/*.md" .||. "fr/**/*.md" .||. "fr/*.html" .||. "en/*.html")    --(((++) (++) <$> (loadAll "en/**/*.md") <*> (loadAll "fr/**/*.md") 
 --------------------------------------------------------------------------------
 -- Reusable blocks
 --
@@ -142,3 +148,54 @@ main = hakyll $ do
     match "templates/**" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
+createSitemap :: Compiler [Item String] -> Rules ()
+createSitemap pages =
+    create ["sitemap.xml"] $ do
+        route idRoute
+        compile $ do
+            let urlCtx = field "urls" (\_ -> urlList pages) `mappend` defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/sitemap.xml" urlCtx
+
+urlList :: Compiler [Item String] -> Compiler String
+urlList compPosts = do
+    posts <- compPosts
+    applyUrlTemplateList posts
+
+applyUrlTemplateList :: [Item String] -> Compiler String
+applyUrlTemplateList posts = do
+    items  <- mapM applyUrlTemplate posts
+    return $ concat $ intersperse "" $ map itemBody items
+
+applyUrlTemplate :: Item String -> Compiler (Item String)
+applyUrlTemplate item = do
+    tpl  <- urlTemplate item
+    applyTemplate tpl urlCtx item
+
+urlTemplate ::  Item a -> Compiler Template
+urlTemplate item = do
+    metadata  <- getMetadata . itemIdentifier $ item
+    loadBody "templates/sitemap_element.xml"
+    
+getFullUrl :: String -> (Item a) -> Compiler String
+getFullUrl root item = do
+    mbPath <- getRoute.itemIdentifier $ item
+    let fullUrl = case mbPath of
+         Nothing  -> ""
+         Just url -> (root ++) . toUrl $ url
+    return fullUrl
+
+getModificationTime :: (Item a) -> Compiler String
+getModificationTime item = do
+    let filePath = toFilePath . itemIdentifier $ item
+    fileStatus <- unsafeCompiler . getFileStatus $ filePath
+    let modTime = posixSecondsToUTCTime . realToFrac . modificationTime $ fileStatus
+    let str = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S+00:00" modTime
+    return str
+
+urlCtx :: Context String
+urlCtx =
+    (field "url" $ getFullUrl "http://www.clever-cloud.com")
+    `mappend` (field "last_modified" getModificationTime)
+    `mappend` dateField "date" "%B %e, %Y"
+    `mappend` defaultContext
